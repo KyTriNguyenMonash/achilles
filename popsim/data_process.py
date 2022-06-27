87f5d51cc70318c7a374a4ac4d608f9fd95c578d
Duc Minh La <dminh.bob.la@gmail.com>
2022-06-27 11:35:02 +1000

    ADD: add the data processing with first take directly from the source

--- popsim/data_process.py
+++ popsim/data_process.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
 if __name__ == "__main__":
-    print("Hello")
+    p_atts = ['PERSID', 'AGE', 'SEX', 'FULLTIMEWORK', 'PARTTIMEWORK', 'CASUALWORK', 'ANYWORK', 'HHID']
+    h_atts = ['HHID', 'HHSIZE', 'CARS', 'CW_ADHHWGT_SA3','HomeSA3']
+    
+    # import data
+    p_original_df = pd.read_csv("./data/source/VISTA_2012_16_v1_SA1_CSV/P_VISTA12_16_SA1_V1.csv")
+    h_original_df = pd.read_csv("./data/source/VISTA_2012_16_v1_SA1_CSV/H_VISTA12_16_SA1_V1.csv")
+
+    p_test_seed = p_original_df[p_atts]
+    h_test_seed = h_original_df[h_atts]
+
+    print(p_test_seed)
+    print(h_test_seed)
