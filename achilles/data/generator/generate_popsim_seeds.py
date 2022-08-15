ec9b200764ae76704c86b13abcb964da815f0656
Duc Minh La <dminh.bob.la@gmail.com>
2022-08-15 15:09:40 +1000

    FIX: refactor the seed generator

--- achilles/data/generator/generate_popsim_seeds.py
+++ achilles/data/generator/generate_popsim_seeds.py
@@ -14,45 +14,46 @@ MATCHING_NAME_CROSS = [
     ("HomeSA4", "SA4_NAME_2016", "SA4_CODE_2016")
 ]
 
-PATH_HTS_PERSONS_12_16_SA1_V1 = "P_VISTA12_16_SA1_V1.csv"
-PATH_HTS_HOUSEHOLDS_12_16_SA1_V1 = "H_VISTA12_16_SA1_V1.csv"
-GEO_FILE = "geo_cross_walk.csv"
+PATH_HTS_PERSONS_12_16_SA1_V1 = "VISTA_2012_16_v1_SA1_CSV/P_VISTA12_16_SA1_V1.csv"
+PATH_HTS_HOUSEHOLDS_12_16_SA1_V1 = "VISTA_2012_16_v1_SA1_CSV/H_VISTA12_16_SA1_V1.csv"
+GEO_FILE = "MB_2016_VIC.csv"
 
 PERSONS_ATTRIBUTES = [
-    "PERSID",
-    "AGE",
-    "SEX",
-    "FULLTIMEWORK",
-    "PARTTIMEWORK",
-    "CASUALWORK",
-    "ANYWORK",
-    "HHID",
-    "HomeSA1", "HomeSA2", "HomeSA3", "HomeSA4"
-]
-
+        "PERSID",
+        "AGE",
+        "SEX",
+        "FULLTIMEWORK",
+        "PARTTIMEWORK",
+        "CASUALWORK",
+        "ANYWORK",
+        "HHID",
+        "HomeSA1", "HomeSA2", "HomeSA3", "HomeSA4"
+    ]
 HOUSEHOLDS_ATTRIBUTES = ["HHID", "HHSIZE", "CARS", "CW_ADHHWGT_SA3","HomeSA1", "HomeSA2", "HomeSA3", "HomeSA4"]
-
+   
 
 @click.command()
 @click.option("-l", "--local-dir", required=True, help="Local data directory")
 @click.option("-o", "--output-dir", required=True, help="Directory for generated data")
 def main(local_dir, output_dir):
+    h_test_seed, p_test_seed = running_final(local_dir)
+    log.info("Generate persons seed")
+    p_test_seed.to_csv(os.path.join(output_dir, f"p_test_seed.csv"), index=False)
 
-    log.info("Load data into memory ....")
-    p_original_df = pd.read_csv(os.path.join(local_dir, PATH_HTS_PERSONS_12_16_SA1_V1))
-    h_original_df = pd.read_csv(
-        os.path.join(local_dir, PATH_HTS_HOUSEHOLDS_12_16_SA1_V1)
-    )
+    log.info("Generate households seed")
+    h_test_seed.to_csv(os.path.join(output_dir, f"h_test_seed.csv"), index=False)
+
+def mod(local_dir):
+    h_test_seed, p_test_seed = running_final(local_dir)
+    return h_test_seed, p_test_seed
+
+def running_final(local_dir):
+    log.info("NOTE: this run is very specific to VIC data")
     geo_df = pd.read_csv(
-        os.path.join(output_dir, GEO_FILE)
+        os.path.join(local_dir, GEO_FILE)
     )
-
-    log.info("Filter persons and households with attributes")
-    p_test_seed = p_original_df[PERSONS_ATTRIBUTES]
-    h_test_seed = h_original_df[HOUSEHOLDS_ATTRIBUTES]
-
-    log.info("Check unique id for persons and households")
-    validate_hts_data(p_test_seed, h_test_seed)
+    h_test_seed = running_hh(local_dir)
+    p_test_seed = running_pp(local_dir)
 
     log.info("Process households")
     h_test_seed = process_households(h_test_seed)
@@ -63,26 +64,36 @@ def main(local_dir, output_dir):
     p_test_seed = process_persons(p_test_seed)
     p_test_seed = replace_name_in_seed(p_test_seed, geo_df, MATCHING_NAME_CROSS).dropna()
     p_test_seed = match_hhid_to_p(p_test_seed, h_test_seed)
+    return h_test_seed, p_test_seed
 
-    log.info("Generate persons seed")
-    p_test_seed.to_csv(os.path.join(output_dir, f"p_test_seed.csv"), index=False)
+def running_pp(local_dir):
+    log.info("Load data people into memory ....")
+    p_original_df = pd.read_csv(
+        os.path.join(local_dir, PATH_HTS_PERSONS_12_16_SA1_V1)
+    )
+    log.info("Filter persons with attributes")
+    p_test_seed = p_original_df[PERSONS_ATTRIBUTES]
+    log.info("Check unique id for persons")
+    if not is_unique_att(p_test_seed, "PERSID"):
+        raise Exception("THE ID OF PERSON IS NOT UNIQUE")
+    return p_test_seed
 
-    log.info("Generate households seed")
-    h_test_seed.to_csv(os.path.join(output_dir, f"h_test_seed.csv"), index=False)
-
+def running_hh(local_dir):
+    log.info("Load data household into memory ....")
+    h_original_df = pd.read_csv(
+        os.path.join(local_dir, PATH_HTS_HOUSEHOLDS_12_16_SA1_V1)
+    )
+    log.info("Filter households with attributes")
+    h_test_seed = h_original_df[HOUSEHOLDS_ATTRIBUTES]
+    log.info("Check unique id for households")
+    if not is_unique_att(h_test_seed, "HHID"):
+        raise Exception("THE ID OF THE HOUSEHOLD IS NOT UNIQUE")
+    return h_test_seed
 
 def is_unique_att(df, unique_att):
     N = df.shape[0]
     test_N = len(df[unique_att].unique())
     return True if N == test_N else False
-
-
-def validate_hts_data(p_test_seed, h_test_seed):
-    if not is_unique_att(p_test_seed, "PERSID"):
-        raise Exception("THE ID OF PERSON IS NOT UNIQUE")
-    if not is_unique_att(h_test_seed, "HHID"):
-        raise Exception("THE ID OF THE HOUSEHOLD IS NOT UNIQUE")
-
 
 def process_persons(p_test_seed):
     p_test_seed.loc[p_test_seed["CASUALWORK"] == "Yes", "ANYWORK"] = "CASUALWORK"
