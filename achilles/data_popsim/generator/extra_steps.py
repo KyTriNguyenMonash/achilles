7bf09bbf58036bced106d27f1daa99b37e21d35d
Duc Minh La <dminh.bob.la@gmail.com>
2022-08-22 12:20:43 +1000

    ADD: reweighting by taking factor, done

--- achilles/data_popsim/generator/extra_steps.py
+++ achilles/data_popsim/generator/extra_steps.py
@@ -11,6 +11,7 @@ def main(output_dir):
     df = getting_sum_totals(output_dir)
     log.info("Generating the csv file for STATE")
     df.to_csv(os.path.join(output_dir, "STATE_all_control.csv"), index=False)
+    refactor_weights_both(output_dir, export_csv=True)
 
 def getting_sum_totals(output_dir):
     log.info("Beginning generating the sums of controls for STATE using SA4 level data")
@@ -57,6 +58,36 @@ def getting_sum_totals(output_dir):
     s["STATE_CODE_2016"] = 2
     fi = pd.DataFrame(s).T
     return fi
+
+def refactor_weights_both(output_dir, export_csv=False):
+    log.info("Reweighting the seed based factoring to controls")
+    log.info("NOTE this needs to run after got the final seed and state/region file")
+    log.info("Loading the needed files into memomy ...")
+    hh_file = "h_test_seed.csv"
+    p_file = "p_test_seed.csv"
+    state_file = "STATE_all_control.csv"
+    df_hh = pd.read_csv(os.path.join(output_dir, hh_file))
+    df_p = pd.read_csv(os.path.join(output_dir, p_file))
+    df_state = pd.read_csv(os.path.join(output_dir, state_file))
+
+    log.info("Start to reweight households")
+    df_hh = reweighting(df_hh, "RP_ADHHWGT_SA3", df_state["Total_dwelings"])
+    log.info("Start to reweight persons")
+    df_p = reweighting(df_p, "RP_ADPERSWGT_SA3", df_state["Tot_P_P"])
+    if export_csv:
+        log.info("Exporting to csv, default is to replace the previous seed files")
+        log.info("Households ...")
+        df_hh.to_csv(os.path.join(output_dir, hh_file), index=False)
+        log.info("Persons ...")
+        df_p.to_csv(os.path.join(output_dir, p_file), index=False)
+    return df_hh, df_p
+
+def reweighting(df_seed, name_weight, target_total):
+    seed_tot = df_seed[name_weight].sum()
+    factor = float(target_total) / float(seed_tot)
+    df_seed[name_weight] = df_seed[name_weight] * factor
+    return df_seed
+
 
 if __name__ == "__main__":
     main()
