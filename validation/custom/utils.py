c1a68e430edf430386def478a914671bab180e21
Duc Minh La <dminh.bob.la@gmail.com>
2022-08-30 11:55:22 +1000

    run for add zone level

--- validation/custom/utils.py
+++ validation/custom/utils.py
@@ -16,28 +16,35 @@ if __name__ == '__main__':
     households = to_df(output_hh_file)
     persons = to_df(output_p_file)
 
-    SA2_tot_file = 'data/SA2_controls.csv'
-    df_tot_SA2 = to_df(SA2_tot_file)
-    spec_ls = ['Total_dwelings', 'Tot_P_P']
-    skip_ls = ['TOTALVEHS']
-    for df, att, exp in zip(df_controls['seed_table'], df_controls['control_field'],  df_controls['expression']):
-        if att in skip_ls: continue
-        if att in spec_ls: continue
-        df = eval(df)
-        filtered_df = df[eval(exp)]
-        zone_lev = 'SA2'
-        zone_tot = 'SA2_MAINCODE_2016'
-        dict_vals_syn = filtered_df[zone_lev].value_counts()
-        x, y = [], []
-        for zone, num in zip(df_tot_SA2[zone_tot], df_tot_SA2[att]):
-            x.append(dict_vals_syn[zone] if zone in dict_vals_syn else 0)
-            y.append(num)
-        base_line = [min(x), min(y), max(x), max(y)]
-        plt.plot(base_line, base_line, color='orange', label='Base line')
-        plt.scatter(x, y, alpha=0.5)
-        plt.xlabel('Synthetic')
-        plt.ylabel('Actual')
-        plt.title(f'Level: {zone_lev} - Att: {att}')
-        plt.legend()
-        plt.show()
+    spec_ls = ['Total_dwelings', 'Tot_P_P'] # These are generated using the weights
+    skip_ls = ['TOTALVEHS'] #Because it does not exist in the synthetic data
+
+    ls_zones_lev = {
+        'SA1': 'SA1_7DIGITCODE_2016',
+        'SA2': 'SA2_MAINCODE_2016',
+        'SA3': 'SA3_CODE_2016'
+    }
+
+    for zone_lev in ls_zones_lev:
+        zone_tot = ls_zones_lev[zone_lev]
+        tot_file = f'data/{zone_lev}_controls.csv'
+        df_tot = to_df(tot_file)
+        for df, att, exp in zip(df_controls['seed_table'], df_controls['control_field'],  df_controls['expression']):
+            if att in skip_ls: continue
+            if att in spec_ls: continue
+            df = eval(df)
+            filtered_df = df[eval(exp)]
+            dict_vals_syn = filtered_df[zone_lev].value_counts()
+            x, y = [], []
+            for zone, num in zip(df_tot[zone_tot], df_tot[att]):
+                x.append(dict_vals_syn[zone] if zone in dict_vals_syn else 0)
+                y.append(num)
+            base_line = [min(x), min(y), max(x), max(y)]
+            plt.plot(base_line, base_line, color='orange', label='Base line')
+            plt.scatter(x, y, alpha=0.5)
+            plt.xlabel('Synthetic')
+            plt.ylabel('Actual')
+            plt.title(f'Level: {zone_lev} - Att: {att}')
+            plt.legend()
+            plt.show()
   
